7c758720d2fb7b30d2d9da1acf4244121a029480
aiziyuer <ziyu0123456789@gmail.com>
2021-03-21 10:37:44 -0400

    项目初始化

---
+++ collections/ansible_collections/ansible/netcommon/plugins/module_utils/network/common/network_template.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.rm_base.network_template import (  # noqa: F401
+    NetworkTemplate,
+)
